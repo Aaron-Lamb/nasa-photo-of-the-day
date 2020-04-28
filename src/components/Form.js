86070fc668603e845eef6f97a8ea4f39f6768f19
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-04-28 16:20:04 -0700

    Starter my form.js file, imported react into it, created the drop form, and exported it.

---
+++ src/components/Form.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const DropForm = () => {
+    return(
+        <div>
+            <form>
+                <label>
+                    Time Machine!
+                    <input type="text" name="dropdown"/>
+                </label>
+            </form>
+        </div>
+    )
+}
+
+export default DropForm
