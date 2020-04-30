974f1839db51a5b6a0da1867030f0756e98e9ee2
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-04-29 19:22:31 -0700

    Styled my image with a set width, height, and padding, giving a border as well. Also styled the h1, setting width, color, and other things.

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,10 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+
+const DayImage = styled.img`
+    width: 75%;
+    height: 40h;
+    border: 2px solid black;
+    padding: 3%;
+`
+const TitleHead = styled.h1`
+    width: 50%;
+    color: white;
+    background-color: black;
+    margin-left: 22.5%;
+    padding: 2%;
+    border: 2px solid skyblue;
+`
 
 const Header = (props) => {
     return(
         <div>
-            <h1>{props.title}</h1>
-            <img src={props.url} alt={props.title}></img>
+            <TitleHead>{props.title}</TitleHead>
+            <DayImage src={props.url} alt={props.title}></DayImage>
         </div>
     )
 }
