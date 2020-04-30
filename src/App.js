1c25c02174e83a658e01769fca6e83c3de86b21f
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-04-29 19:26:31 -0700

    Set the background image to cover the whole app div.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import styled from "styled-components";
 import skyImage from "./taylor-van-riper-yQorCngxzwI-unsplash.jpg"
 
 const AppDiv = styled.div`
-  background-image: url(${skyImage})
+  background-image: url(${skyImage});
+  background-size: cover;
 `
 
 function App() {
