42f6195e2fef21b2211a2a36f2f6bc954fe44b7c
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-04-29 18:18:01 -0700

    Downloaded a free image of nasa for my component

--- src/App.js
+++ src/App.js
@@ -18,12 +18,8 @@ function App() {
   }, [])
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-      <DropForm />
       <Header title={data.title} url={data.url}/>
+      <DropForm />
       <Content date={data.date} explanation={data.explanation}/>
     </div>
   );
