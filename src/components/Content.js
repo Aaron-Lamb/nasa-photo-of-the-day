d4f1c666afa4658a27ec793914ee4acb62eb0761
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-04-30 17:40:03 -0700

    Refactored the paragraph styling in Content to emotion.

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,22 +1,26 @@
 import React from "react";
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
 import styled from "styled-components";
 
 const DateHead = styled.h3`
 font-family: 'Baloo Tamma 2', cursive;
-`
-const InfoPar = styled.p`
-    margin-left: 12.25%;
-    line-height: 150%;
-    width: 75%;
-    border: 3px solid grey;
-    background-color: aliceblue;
 `
 
 const Content =(props) => {
     return(
         <div>
             <DateHead>{props.date}</DateHead>
-            <InfoPar>{props.explanation}</InfoPar>
+            <p
+            css={{
+                marginLeft: '12.25%',
+                lineHeight: '150%',
+                width: '75%',
+                border: '3px solid grey',
+                backgroundColor: 'aliceblue'
+            }}
+            {...props}
+            >{props.explanation}</p>
         </div>
     )
 }
