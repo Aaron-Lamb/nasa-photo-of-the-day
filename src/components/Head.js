ccae3f43cef99fae0c5063af9de2a2562d1f57ce
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-04-30 17:22:54 -0700

    Transformed all of my previous styling in the Head file, aside from the font family, into emotion.

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,30 +1,38 @@
 import React from "react";
 import styled from "styled-components";
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
 
-const DayImage = styled.img`
-    width: 75%;
-    height: 40h;
-    border: 2px solid black;
-    border-radius: 3%;
-    background-color: white;
-    background: rgba(0, 0, 0, 0.2);
-    padding: 3%;
-`
-const TitleHead = styled.h1`
-    width: 50%;
-    color: white;
-    background-color: black;
-    margin-left: 22.5%;
-    padding: 2%;
-    border: 2px solid skyblue;
-    font-family: 'Baloo Tamma 2', cursive;
+const HeadFont = styled.h1`
+font-family: 'Baloo Tamma 2', cursive;
 `
 
 const Header = (props) => {
     return(
         <div>
-            <TitleHead>{props.title}</TitleHead>
-            <DayImage src={props.url} alt={props.title}></DayImage>
+            <HeadFont
+            css={{
+                width: '50%',
+                color: 'white',
+                backgroundColor: 'black',
+                marginLeft: '22.5%',
+                padding: '2%',
+                border: '2px solid skyblue'
+                }}
+            {...props}
+            >{props.title}</HeadFont>
+            <img 
+            css={{
+                width: '75%',
+                height: '40h',
+                border: '2px solid black',
+                borderRadius: '3%',
+                backgroundColor: 'white',
+                background: 'rgba(0, 0, 0, 0.2)',
+                padding: '3%'
+            }}
+            {...props}
+            src={props.url} alt={props.title}></img>
         </div>
     )
 }
